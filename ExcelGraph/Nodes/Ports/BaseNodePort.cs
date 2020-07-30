4548f87a4ba373c5723dec1abed07e3ebcb58840
sdhexu@botj <sdhexu@hotmail.com>
2020-07-30 14:43:42 +0800

    aa

---
+++ ExcelGraph/Nodes/Ports/BaseNodePort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ExcelGraph.Nodes.Ports
+{
+    public abstract class BaseNodePort
+    {
+        public BaseNode Belong { get; set; } = null;
+        public string Label { get; set; } = null;
+        public abstract void OnDraw(Graphics g);
+    }
+}
